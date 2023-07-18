d1575e8bd665f0496b6003e1fa48e4bd6d91b831
eymontesinos <135139454+eymontesinos@users.noreply.github.com>
2023-07-18 15:42:17 -0500

    add two link

--- IosCourseSwiftUi/presentation/login/LoginScreen.swift
+++ IosCourseSwiftUi/presentation/login/LoginScreen.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 struct LoginScreen: View {
+    var title : String = ""
     var body: some View {
         NavigationView{
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        }.navigationTitle("Login").navigationBarBackButtonHidden(true)
+            Text(title)
+        }.navigationTitle("Hola login").navigationBarBackButtonHidden(true)
       
     }
 }
--- IosCourseSwiftUi/presentation/splash/SplashScreen.swift
+++ IosCourseSwiftUi/presentation/splash/SplashScreen.swift
@@ -10,15 +10,24 @@ import CoreData
 
 struct SplashScreen: View {
     
+    @State var nextLogin : Bool = false
+    
     var body: some View {
         NavigationView {
             VStack(spacing: 16) {
-                Text("FOOD MERIA").font(.system(size: 25,weight: .bold)).foregroundColor(.white)
-                Text("BIENVENIDOS").font(.system(size: 20,weight: .bold)) .foregroundColor(.white)
-                Text("HAS TU PEDIDO AHORA").font(.system(size: 20,weight: .bold)) .foregroundColor(.white)
-                DelayedNavigationLink(delay: .seconds(3)) {
-                    LoginScreen()
+                Button(action:{ nextLogin = true}){
+                    Text("FOOD MERIA").font(.system(size: 25,weight: .bold)).foregroundColor(.white)
+                }.sheet(isPresented: $nextLogin, content: {
+                    LoginScreen(title: "Hola login")
+                })
+                
+                NavigationLink(destination: LoginScreen(title: "Hola login")){
+                    Text("BIENVENIDOS").font(.system(size: 20,weight: .bold)) .foregroundColor(.white)
                 }
+                
+              
+                Text("HAS TU PEDIDO AHORA").font(.system(size: 20,weight: .bold)) .foregroundColor(.white)
+               // DelayedNavigationLink(delay: .seconds(3)) {LoginScreen()}
             }.edgesIgnoringSafeArea(.all).frame(maxWidth: .infinity,maxHeight: .infinity)
                 .background(
                     Image(uiImage: UIImage(named: "background_splash")!)
