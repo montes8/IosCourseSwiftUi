7fb1e0ee5482523c67705a9d4bbd98717aa12658
eymontesinos <135139454+eymontesinos@users.noreply.github.com>
2023-07-19 09:54:20 -0500

    delete code not use

--- IosCourseSwiftUi/presentation/splash/SplashScreen.swift
+++ IosCourseSwiftUi/presentation/splash/SplashScreen.swift
@@ -17,26 +17,17 @@ struct SplashScreen: View {
     var body: some View {
         NavigationView {
             VStack(spacing: 16) {
-                Button(action:{ nextLogin = true}){
-                    Text("FOOD MERIA").font(.system(size: 25,weight: .bold)).foregroundColor(.white)
-                }.sheet(isPresented: $nextLogin, content: {
-                    LoginScreen(title: "Hola login")
-                })
-                
-                NavigationLink(destination: LoginScreen(title: "Hola login"),isActive: $nextLogin){
-                    Text("BIENVENIDOS").font(.system(size: 20,weight: .bold)) .foregroundColor(.white)
-                }
-                
-              
+                Text("FOOD MERIA").font(.system(size: 25,weight: .bold)).foregroundColor(.white)
+                Text("BIENVENIDOS").font(.system(size: 20,weight: .bold)) .foregroundColor(.white)
                 Text("HAS TU PEDIDO AHORA").font(.system(size: 20,weight: .bold)) .foregroundColor(.white)
-               // DelayedNavigationLink(delay: .seconds(3)) {LoginScreen()}
-                //if splashViewModel != nil{
-                //    if splashViewModel.successToken ?? false{
-                //        DelayedNavigationLink(delay: .seconds(3)) {LoginScreen()}
-                //   }else{
-                //       DelayedNavigationLink(delay: .seconds(3)) {LoginScreen()}
-                //    }
-                // }
+              
+                if splashViewModel != nil{
+                    if splashViewModel.successToken ?? false{
+                        DelayedNavigationLink(delay: .seconds(3)) {LoginScreen()}
+                   }else{
+                       DelayedNavigationLink(delay: .seconds(3)) {LoginScreen()}
+                    }
+                }
               
             }.edgesIgnoringSafeArea(.all).frame(maxWidth: .infinity,maxHeight: .infinity)
                 .background(
@@ -47,8 +38,6 @@ struct SplashScreen: View {
         }
         
     }
-   
-        
        
 }
 
